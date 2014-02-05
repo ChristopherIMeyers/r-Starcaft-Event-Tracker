b8a662650d9c7d80f24d03080bb5fd8a97d75ed5
Christopher Meyers <Joe.Master@att.net>
2014-02-04 20:41:01 -0800

    sanity test to prove praw is running

--- sanity.py
+++ sanity.py
@@ -1,7 +1,10 @@
 import unittest
+import praw
 class CheckSanity(unittest.TestCase):
-  def test_bad(self):
-    self.assertTrue(False)
+  def test_praw(self):
+    r = praw.Reddit(user_agent='r/starcraft event tracker')
+    frontpage = r.get_front_page()
+    self.assertEqual(sum(1 for _ in frontpage), 25)
 
 if __name__ == '__main__':
   unittest.main()
