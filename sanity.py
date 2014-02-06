0edff43d229f0fdcbafac72e4514ca5c2f156928
Christopher Meyers <Joe.Master@att.net>
2014-02-05 21:47:53 -0800

    sanity check of httplib
    verify we can ready data from teamliquid

--- sanity.py
+++ sanity.py
@@ -1,10 +1,23 @@
 import unittest
 import praw
+import httplib
+
 class CheckSanity(unittest.TestCase):
   def test_praw(self):
     r = praw.Reddit(user_agent='r/starcraft event tracker')
     frontpage = r.get_front_page()
     self.assertEqual(sum(1 for _ in frontpage), 25)
+
+  def test_http(self):
+    conn = httplib.HTTPConnection('wiki.teamliquid.net')
+    conn.connect()
+    request = conn.putrequest('GET','/starcraft2/api.php?format=txt&action=query&titles=Liquipedia:Tournament_News&prop=revisions&rvprop=content')
+    conn.endheaders()
+    conn.send('')
+    resp = conn.getresponse()
+    #self.assertTrue(resp.read() == '')
+    self.assertTrue(len(resp.read()) > 4000)
+    
 
 if __name__ == '__main__':
   unittest.main()
