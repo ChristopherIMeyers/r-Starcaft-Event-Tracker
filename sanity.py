4cb308d509495d0e348266b14b573c30b351d954
Christopher Meyers <Joe.Master@att.net>
2014-02-06 19:00:11 -0800

    refactoring sanity tests

--- sanity.py
+++ sanity.py
@@ -2,6 +2,14 @@ import unittest
 import praw
 import httplib
 
+def getLiquipediaEvents():
+  conn = httplib.HTTPConnection('wiki.teamliquid.net')
+  conn.connect()
+  request = conn.putrequest('GET','/starcraft2/api.php?format=txt&action=query&titles=Liquipedia:Tournament_News&prop=revisions&rvprop=content')
+  conn.endheaders()
+  conn.send('')
+  return conn.getresponse().read()
+
 class CheckSanity(unittest.TestCase):
   def test_praw(self):
     r = praw.Reddit(user_agent='r/starcraft event tracker')
@@ -9,14 +17,7 @@ class CheckSanity(unittest.TestCase):
     self.assertEqual(sum(1 for _ in frontpage), 25)
 
   def test_http(self):
-    conn = httplib.HTTPConnection('wiki.teamliquid.net')
-    conn.connect()
-    request = conn.putrequest('GET','/starcraft2/api.php?format=txt&action=query&titles=Liquipedia:Tournament_News&prop=revisions&rvprop=content')
-    conn.endheaders()
-    conn.send('')
-    resp = conn.getresponse()
-    #self.assertTrue(resp.read() == '')
-    self.assertTrue(len(resp.read()) > 4000)
+    self.assertTrue(len(getLiquipediaEvents()) > 4000)
     
 
 if __name__ == '__main__':
