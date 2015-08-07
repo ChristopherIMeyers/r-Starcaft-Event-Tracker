dc59d57515876b699be73368148f1d8f253a00a0
Christopher Meyers <Joe.Master@att.net>
2015-08-06 17:38:13 -0700

    update user agent
    resolved praw warnings about user agent string

--- updatesidebar.py
+++ updatesidebar.py
@@ -2,7 +2,7 @@ import praw
 import functions
 import settings
 
-prawLogin = praw.Reddit(user_agent='r/starcraft event tracker bot')
+prawLogin = praw.Reddit(user_agent='r/starcraft event tracker script')
 prawLogin.login(settings.reddituser, settings.redditpass)
 
 functions.setSidebar(prawLogin)
--- updatewiki.py
+++ updatewiki.py
@@ -2,7 +2,7 @@ import praw
 import functions
 import settings
 
-r = praw.Reddit(user_agent='r/starcraft event tracker bot')
+r = praw.Reddit(user_agent='r/starcraft event tracker script')
 r.login(settings.reddituser, settings.redditpass)
 
 functions.setWikiPage(r)
